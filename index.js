b6359c4fe37db517d7e2a4f8ddfbedbbb9d59377
dedunudr <drdedunu@gmail.com>
2025-05-10 12:16:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star TONIC-MD
+Don't Forget To Give Star SOLO-LEVELING-MD
 
  Server running on http://localhost:` + PORT)
 })
